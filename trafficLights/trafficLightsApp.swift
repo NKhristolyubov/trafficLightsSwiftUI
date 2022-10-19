cc2cd6f77694cd5a09788fbaf1c0d314381c0db7
NKhristolyubov <n.khristolyubov@yandex.ru>
2022-10-19 10:36:39 +0400

    Initial Commit

---
+++ trafficLights/trafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightsApp.swift
+//  trafficLights
+//
+//  Created by Николай Христолюбов on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
